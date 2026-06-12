261d46954d16eca3be541a411bef1f85be73331c
Rashoj Onta <rashojonta48@gmail.com>
2026-06-12 11:41:34 -0400

    changes in userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,6 @@ export const getUsers = async () => {
   return response.data
 }
 export async function registerUser(payload) {
-  const response = await api.post("/auth/register", payload)
+  const response = await api.post("/users", payload)
   return response.data
 }
